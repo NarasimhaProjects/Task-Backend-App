9785cbd1183417722d73f1a508f3ee21f06d2711
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-08-17 12:02:20 +0530

    Fresh Commit

---
+++ routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getTasks, setTask, updateTask, deleteTask } = require('../controllers/taskControllers');
+const router = express.Router();
+
+
+router.get('/', getTasks);
+router.post('/', setTask);
+router.put('/:id', updateTask)
+router.delete('/:id', deleteTask)
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const { errorHandler } = require('./middleware/errorMIddleware')
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db');
-const port = process.env.PORT || 5000
-const bookRoutes = require('./routes/books');
+const {errorHandler} = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv').config();
+const connectDB = require('./connect/database')
+const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false}));
 
-app.use('/api/Books', bookRoutes);
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is Listening on ${port}`));
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
