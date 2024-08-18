54326ea6860877c9e674534a6d3a9864c10a8cb0
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-08-18 12:30:55 +0530

    Files modified

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getTasks, setTask, updateTask, deleteTask } = require('../controllers/taskController');
 const router = express.Router();
+const { protect } = require('../middleware/authmiddleware')
 
 
-router.get('/', getTasks);
-router.post('/', setTask);
-router.put('/:id', updateTask)
-router.delete('/:id', deleteTask)
+router.get('/', protect, getTasks);
+router.post('/', protect, setTask);
+router.put('/:id', protect, updateTask)
+router.delete('/:id', protect, deleteTask)
 
 
 module.exports = router;
