95b9846291901c5dc94fdb1558b2bf3564ad26dc
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-08-28 16:36:58 +0530

    ch

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const connectDB = require(`./connect/database`);
 const Cors = require('cors');
 const port = process.env.PORT || 5000;
-const errorHandler = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 connectDB();
 const app = express();
